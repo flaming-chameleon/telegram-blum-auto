46dd998a7306da180f506138676e637881d0141b
Flaming Chameleon <73156836+flaming-chameleon@users.noreply.github.com>
2024-05-27 16:05:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
-from utils.core import create_sessions
-from utils.telegram import Accounts
-from utils.starter import start, stats
-import asyncio
-from itertools import zip_longest
-from utils.core import get_all_lines
-import os
-
+from utils.core import create_sessions  # Import function to create pyrogram sessions
+from utils.telegram import Accounts  # Import Accounts class to manage Telegram accounts
+from utils.starter import start, stats  # Import functions to start the process and get statistics
+import asyncio  # Import asyncio for asynchronous programming
+from itertools import zip_longest  # Import zip_longest for pairing accounts with proxies
+from utils.core import get_all_lines  # Import function to read all lines from a file
+import os  # Import os for interacting with the operating system
 
 async def main():
+    # Print creator's information
     print("Soft created by: https://t.me/hidden_coding\n")
+    
+    # Get user input for selecting an action
     action = int(input("Select action:\n1. Start soft\n2. Get statistics\n3. Create pyrogram sessions\n\n> "))
 
+    # Create directories if they do not exist
     if not os.path.exists('sessions'): os.mkdir('sessions')
     if not os.path.exists('statistics'): os.mkdir('statistics')
 
+    # Create pyrogram sessions
     if action == 3:
         await create_sessions()
 
+    # Get statistics
     if action == 2:
         await stats()
 
+    # Start the main process
     if action == 1:
+        # Get accounts from the Accounts class
         accounts = await Accounts().get_accounts()
+        # Read proxies from file
         proxys = get_all_lines("data/proxy.txt")
 
         tasks = []
+        # Iterate over accounts and proxies, creating a task for each pair
         for thread, (account, proxy) in enumerate(zip_longest(accounts, proxys)):
-            if not account: break
+            if not account: break  # Exit loop if no more accounts
+            # Create and append the task to the tasks list
             tasks.append(asyncio.create_task(start(account=account, thread=thread, proxy=proxy)))
 
+        # Run all tasks concurrently
         await asyncio.gather(*tasks)
 
-
 if __name__ == '__main__':
-    
+    # Print application banner
     print("""
           
 
@@ -48,5 +58,5 @@ if __name__ == '__main__':
           
           """)
     
-    
+    # Run the main asynchronous function
     asyncio.get_event_loop().run_until_complete(main())
