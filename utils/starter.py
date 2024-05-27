cf218524a5efc62bfb7edfd0e56e249502cc081a
Flaming Chameleon <73156836+flaming-chameleon@users.noreply.github.com>
2024-05-27 17:03:35 +0200

    Update starter.py

--- utils/starter.py
+++ utils/starter.py
@@ -12,55 +12,56 @@ from utils.core import get_all_lines
 
 
 async def start(thread: int, account: str, proxy: [str, None]):
-    blum = BlumBot(account=account, thread=thread, proxy=proxy)
-
-    await sleep(uniform(*config.DELAYS['ACCOUNT']))
-    await blum.login()
-    await blum.stats()
-
-    while True:
-        try:
-            msg = await blum.claim_daily_reward()
-            if isinstance(msg, bool) and msg:
-                logger.success(f"Thread {thread} | Claimed daily reward!")
-
-            timestamp, start_time, end_time, play_passes = await blum.balance()
-
-            await blum.play_game(play_passes)
-            await sleep(uniform(5, 10))
-
-            for task in await blum.get_tasks():
-                if task['status'] == 'CLAIMED' or task['title'] in config.BLACKLIST_TASKS: continue
-
-                if task['status'] == "NOT_STARTED":
-                    await blum.start_complete_task(task)
-                    await sleep(uniform(10, 15))
-                elif task['status'] == 'STARTED':
-                    await sleep(uniform(10, 15))
-
-                if await blum.claim_task(task):
-                    logger.success(f"Thread {thread} | Completed task «{task['title']}»")
+    async with CloudflareScraper(headers={'User-Agent': UserAgent(os='android').random}, timeout = aiohttp.ClientTimeout(total=60)) as session:
+        blum = BlumBot(account=account, thread=thread, session=session, proxy=proxy)
+    
+        await sleep(uniform(*config.DELAYS['ACCOUNT']))
+        await blum.login()
+        #await blum.stats()
+    
+        while True:
+            try:
+                msg = await blum.claim_daily_reward()
+                if isinstance(msg, bool) and msg:
+                    logger.success(f"Thread {thread} | Claimed daily reward!")
+    
+                timestamp, start_time, end_time, play_passes = await blum.balance()
+    
+                await blum.play_game(play_passes)
+                await sleep(uniform(5, 10))
+    
+                for task in await blum.get_tasks():
+                    if task['status'] == 'CLAIMED' or task['title'] in config.BLACKLIST_TASKS: continue
+    
+                    if task['status'] == "NOT_STARTED":
+                        await blum.start_complete_task(task)
+                        await sleep(uniform(10, 15))
+                    elif task['status'] == 'STARTED':
+                        await sleep(uniform(10, 15))
+    
+                    if await blum.claim_task(task):
+                        logger.success(f"Thread {thread} | Completed task «{task['title']}»")
+                    else:
+                        logger.error(f"Thread {thread} | Failed complete task «{task['title']}»")
+    
+                timestamp, start_time, end_time, play_passes = await blum.balance()
+    
+                if start_time is None and end_time is None:
+                    await blum.start()
+                    logger.info(f"Thread {thread} | Start farming!")
+    
+                elif start_time is not None and end_time is not None and timestamp >= end_time:
+                    await blum.refresh()
+                    timestamp, balance = await blum.claim()
+                    logger.success(f"Thread {thread} | Claimed reward! Balance: {balance}")
+    
                 else:
-                    logger.error(f"Thread {thread} | Failed complete task «{task['title']}»")
-
-            timestamp, start_time, end_time, play_passes = await blum.balance()
-
-            if start_time is None and end_time is None:
-                await blum.start()
-                logger.info(f"Thread {thread} | Start farming!")
-
-            elif start_time is not None and end_time is not None and timestamp >= end_time:
-                await blum.refresh()
-                timestamp, balance = await blum.claim()
-                logger.success(f"Thread {thread} | Claimed reward! Balance: {balance}")
-
-            else:
-                logger.info(f"Thread {thread} | Sleep {end_time - timestamp} seconds!")
-                await sleep(end_time - timestamp)
-
-            await sleep(10)
-        except Exception as e:
-            logger.error(f"Thread {thread} | Error: {e}")
+                    logger.info(f"Thread {thread} | Sleep {end_time - timestamp} seconds!")
+                    await sleep(end_time - timestamp)
+    
+                await sleep(10)
+            except Exception as e:
+                logger.error(f"Thread {thread} | Error: {e}")
 
 
 async def stats():
