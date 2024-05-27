51c6f830be4e9303222fd542ae862b8eeb389c06
Flaming Chameleon <73156836+flaming-chameleon@users.noreply.github.com>
2024-05-27 18:46:48 +0200

    For WIndows users ease usage

---
+++ INSTALL.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Creating virtual environment...
+python -m venv venv
+echo Activating virtual environment...
+call venv\Scripts\activate
+echo Installing dependencies...
+pip install -r requirements.txt
+echo done
+pause
