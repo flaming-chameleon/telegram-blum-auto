0f77e869fe9c5f92eecc461a6bfa11bd67ee95cb
AlexKrutoy <lollololololoolll@gmail.com>
2024-06-03 15:07:15 +0300

    Fix farming i guess

--- utils/starter.py
+++ utils/starter.py
@@ -12,58 +12,71 @@ from utils.helper import format_duration
 
 
 async def start(thread: int, account: str, proxy: [str, None]):
-    async with CloudflareScraper(headers={'User-Agent': UserAgent(os='android').random},
-                                 timeout=aiohttp.ClientTimeout(total=60)) as session:
-        blum = BlumBot(account=account, thread=thread, session=session, proxy=proxy)
-        max_try = 5
-    
-        await sleep(uniform(*config.DELAYS['ACCOUNT']))
-        await blum.login()
-    
-        while True:
+    while True:
+        async with CloudflareScraper(headers={'User-Agent': UserAgent(os='android').random},
+                                     timeout=aiohttp.ClientTimeout(total=60)) as session:
             try:
-                msg = await blum.claim_daily_reward()
-                if isinstance(msg, bool) and msg:
-                    logger.success(f"{account} | Claimed daily reward!")
+                blum = BlumBot(account=account, thread=thread, session=session, proxy=proxy)
+                max_try = 2
 
-                timestamp, start_time, end_time, play_passes = await blum.balance()
+                await sleep(uniform(*config.DELAYS['ACCOUNT']))
+                await blum.login()
 
-                if play_passes and play_passes > 0 and config.PLAY_GAMES is True:
-                    await blum.play_game(play_passes)
-                elif config.PLAY_GAMES is False:
-                    play_passes = 0
+                while True:
+                    try:
+                        msg = await blum.claim_daily_reward()
+                        if isinstance(msg, bool) and msg:
+                            logger.success(f"{account} | Claimed daily reward!")
 
-                await sleep(uniform(3, 10))
+                        timestamp, start_time, end_time, play_passes = await blum.balance()
 
-                timestamp, start_time, end_time, play_passes = await blum.balance()
+                        claim_amount, is_available = await blum.friend_balance()
+                        # logger.info(f"{account} | {claim_amount} | {is_available}")
+                        if claim_amount != 0 and is_available:
+                            amount = await blum.friend_claim()
+                            logger.success(f"Claimed friend ref reward {amount}")
 
-                try:
-                    if start_time is None and end_time is None and max_try > 0:
-                        await blum.start()
-                        logger.info(f"{account} | Start farming!")
-                        max_try -= 1
+                        if config.PLAY_GAMES is False:
+                            play_passes = 0
+                        elif play_passes and play_passes > 0 and config.PLAY_GAMES is True:
+                            await blum.play_game(play_passes)
 
-                    elif start_time is not None and end_time is not None and timestamp is not None and timestamp >= end_time and max_try > 0:
-                        await blum.refresh()
-                        timestamp, balance = await blum.claim()
-                        logger.success(f"{account} | Claimed reward! Balance: {balance}")
-                        max_try -= 1
+                        await sleep(uniform(3, 10))
 
-                    elif end_time is not None and timestamp is not None:
-                        sleep_duration = end_time - timestamp
-                        logger.info(f"{account} | Sleep {format_duration(sleep_duration)}")
-                        max_try += 5
-                        await sleep(sleep_duration)
+                        try:
+                            timestamp, start_time, end_time, play_passes = await blum.balance()
+                            if start_time is None and end_time is None and max_try > 0:
+                                await blum.start()
+                                logger.info(f"{account} | Start farming!")
+                                max_try -= 1
 
-                    elif max_try == 0:
-                        break
+                            elif start_time is not None and end_time is not None and timestamp is not None and timestamp >= end_time and max_try > 0:
+                                await blum.refresh()
+                                timestamp, balance = await blum.claim()
+                                logger.success(f"{account} | Claimed reward! Balance: {balance}")
+                                max_try -= 1
 
-                except Exception as e:
-                    logger.error(f"{account} | Error in farming management: {e}")
+                            elif end_time is not None and timestamp is not None:
+                                sleep_duration = end_time - timestamp
+                                logger.info(f"{account} | Sleep {format_duration(sleep_duration)}")
+                                max_try += 1
+                                await sleep(sleep_duration)
+                                await blum.refresh()
 
-                await sleep(10)
-            except Exception as e:
-                logger.error(f"{account} | Error: {e}")
+                            elif max_try == 0:
+                                break
+
+                        except Exception as e:
+                            logger.error(f"{account} | Error in farming management: {e}")
+
+                        await sleep(10)
+                    except Exception as e:
+                        logger.error(f"{account} | Error: {e}")
+            except Exception as outer_e:
+                logger.error(f"{account} | Session error: {outer_e}")
+            finally:
+                logger.info(f"{account} | Reconnecting, 61 s")
+                await sleep(61)
 
 
 async def stats():
